a9759eda07dcb58f5c7afffdaa340df2610506a4
mrodriguez0411 <96866785+mrodriguez0411@users.noreply.github.com>
2025-02-11 14:52:11 -0300

    Update Loader.tsx

--- src/components/shared/Loader.tsx
+++ src/components/shared/Loader.tsx
@@ -1,9 +1,10 @@
 import { TbFidgetSpinner } from "react-icons/tb"
 
+
 export const Loader = () => {
   return (
     <div className="flex items-center justify-center h-screen">
-        <TbFidgetSpinner className="animate-spin" size={70}/>
+        <TbFidgetSpinner  className="animate-spin" size={70}/>
     </div>
   )
 }
