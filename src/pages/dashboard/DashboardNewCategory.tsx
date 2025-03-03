ad3419081104819a52cf91452f3b9a56a75b6c5c
mrodriguez0411 <96866785+mrodriguez0411@users.noreply.github.com>
2025-03-03 13:23:56 -0300

    Delete DashboardNewCategory.tsx

--- src/pages/dashboard/DashboardNewCategory.tsx
+++
@@ -1,9 +0,0 @@
-import { ManageTypeCategories } from "'./ManageTypeCategories'"; // Asegúrate de que la ruta sea correcta
-
-export const DashboardNewCategory = () => {
-  return (
-    <div>
-      <ManageTypeCategories titleForm="Agregar Producto" />
-    </div>
-  );
-};
